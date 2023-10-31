3ddfba7a5718fd5abd20ccc83f852c1263e0044b
mikecurious <mikkohbrayoh@gmail.com>
2023-10-31 16:02:21 +0300

    integers

---
+++ lesson_2/main2.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//string
+
+	var nameOne string = "mike"
+	var nameTwo = "brian"
+	var nameThree = "Born"
+	var nameFour string
+	fmt.Println(nameOne, nameTwo)
+
+	nameOne = "Muthee"
+	nameFour = "again"
+
+	fmt.Println(nameThree, nameFour)
+
+	nameFive := "Baba Julia"
+
+	fmt.Println(nameFive)
+
+	//ints
+
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+}
